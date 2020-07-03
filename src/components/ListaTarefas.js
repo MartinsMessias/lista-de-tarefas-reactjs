7e8be94c5353f632ace3f90db37858f4a7c2477c
MartinsMessias <messiasads2017@gmail.com>
2020-07-02 22:33:42 -0500

    Adicionando tarefas

--- src/components/ListaTarefas.js
+++ src/components/ListaTarefas.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
+import ClasseTarefas from './ClasseTarefas';
 
 const ListaTarefas = () => {
   return (
     <div>
       <Header />
+      <ClasseTarefas />
     </div>
   );
 }
