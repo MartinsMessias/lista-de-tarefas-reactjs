f08c5619c0a6e60178d237e36190cc782753ec67
MartinsMessias <messiasads2017@gmail.com>
2020-07-02 21:52:29 -0500

    Criaçao

---
+++ src/components/ListaTarefas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function ListaTarefas() {
+  return (
+    <div>
+      Olá mundão
+    </div>
+  );
+}
+
+export default ListaTarefas;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ListaTarefas from './components/ListaTarefas';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ListaTarefas />
   </React.StrictMode>,
   document.getElementById('root')
 );
