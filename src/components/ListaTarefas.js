9fb4d34dd72467d25e068c5b0e649d45a0da381b
MartinsMessias <messiasads2017@gmail.com>
2020-07-02 22:05:06 -0500

    Jumbotron

--- src/components/ListaTarefas.js
+++ src/components/ListaTarefas.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-function ListaTarefas() {
+const ListaTarefas = () => {
   return (
-    <div>
-      Olá mundão
+    <div className="container mt-2">
+      <Jumbotron className="text-center">
+        <h1 className="display-3">Lista de Tarefas</h1>
+        <p className="lead">Uma aplicação simples usando ReactJS, para refrescar nossa memória das tarefas pendentes.</p>
+      </Jumbotron>
     </div>
   );
 }
